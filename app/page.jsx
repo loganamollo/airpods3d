89206a641efdfa3669e159a3259f498a605e3482
loganamollo <loganamollo@gmail.com>
2023-08-24 05:18:23 +0300

    did simple page

--- app/page.jsx
+++ app/page.jsx
@@ -1,62 +1,21 @@
 'use client'
 import Spline from '@splinetool/react-spline'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+import { FaArrowDown } from 'react-icons/fa'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 
 
 export default function Home() {
-    useEffect(() => {
-        // gsap.registerPlugin(ScrollTrigger)
-        // ScrollTrigger.defaults({
-        //     toggleActions: 'restart pause resume pause',
-        //     scroller: '.scroll-container'
-        // })
-        // gsap.from('.scroll-container', {
-        //     scrollTrigger: '.scroll-container',
-        //     xPercent: -100,
-        // })
-
-        // gsap.from('airpods', {
-        //     scrollTrigger: 'airpods',
-        //     xPercent: -100,
-
-        // })
-        // gsap.from('.scroll-item', {
-        //     scrollTrigger: '.scroll-section',
-        //     xPercent: -100
-        // })
-
-        // gsap.from('.scroll-section', {
-        //     xPercent: -100,
-        //     ease: 'power2.inOut',
-        //     scrollTrigger: {
-        //         trigger:  '.scroll-container',
-        //         scrub: 0.5,
-        //         pin: '.airpods',
-        //         start: 'top top',
-        //         end: '+=200',
-        //         snap: 1 / 3 // numSections - 1
-        //     }
-        // })
-    }, [])
-
-
     return (
-        <main className='w-screen h-full grid grid-cols-2'>
-            <div className='bg-black text-white h-screen w-full p-[3rem]'>
-                <h1 className='font-bold text-[3rem]'>Airpods Pro Image</h1>
+        <main className='w-screen h-full relative'>
+            <div className='z-0 h-screen px-8 bg-gradient-to-b from-teal-100 to-fuchsia-200'>
+                <h1 className='font-bold text-[11rem] leading-[-2rem]'>Airpods Pro</h1>
+                <p className='text-[2rem]'>Sounds like a <br /> live performance.</p>
+                {/* <FaArrowDown size={32} className='mx-auto mt-10' /> */}
             </div>
-            <div className='w-full h-screen overflow-y-scroll'>
-                <div className='h-screen w-full p-[3rem]'>
-                    <h1 className='font-bold text-[3rem]'>Airpods Pro Image</h1>
-                </div>
-                <div className='h-screen w-full p-[3rem]'>
-                    <h1 className='font-bold text-[3rem]'>Airpods Pro Image</h1>
-                </div>
-                <div className='h-screen w-full p-[3rem]'>
-                    <h1 className='font-bold text-[3rem]'>Airpods Pro Image</h1>
-                </div>
+            <div className='h-screen w-[80vw]  z-10 absolute top-0 right-0  p-0'>
+                <Spline scene="https://prod.spline.design/MM8img0rXgwWIhfy/scene.splinecode" />
             </div>
         </main>
     )
